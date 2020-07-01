2f1041ec87b8702bf0d515f23dc02479617cb7d2
Thomas Cannon <tcannon.mail@gmail.com>
2020-07-01 13:29:40 -0400

    finished selection

--- GRBCluster/grb_cluster.py
+++ GRBCluster/grb_cluster.py
@@ -6,14 +6,19 @@ print('works')
 
 with open('/home/thomas/data/batse/ascii_data/64ms/burst_files.csv', newline='') as burstfile:
     base_path = '/home/thomas/data/batse/ascii_data/64ms/'
-    for row in csv.reader(burstfile, delimiter=' ', quotechar='|'):
-        file_path = base_path+row[1]
-        print(file_path)
+    for row in csv.reader(burstfile, delimiter=',', quotechar='|'):
 
-        grb = Burst(file_path)
-        grb.parse_batse_file()
+        # if int(row[0]) < 3580:
+        if int(row[0]) != 7812:
+            next
+        else:
+            file_path = base_path+row[1]
+            print(file_path)
 
-        burst_data = grb.sum_chan_data
+            grb = Burst(file_path)
+            grb.parse_batse_file()
 
-        plt.plot(burst_data)
-        plt.show()
+            burst_data = grb.sum_chan_data
+
+            plt.plot(burst_data)
+            plt.show()
