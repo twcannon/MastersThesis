550917669e01c9b40aba6256717a895e7a95bf9d
twcannon <tcannon.mail@gmail.com>
2020-07-02 14:53:31 -0400

    quick commit

--- GRBCluster/grb_cluster.py
+++ GRBCluster/grb_cluster.py
@@ -2,23 +2,78 @@ import scipy
 from grbpy.burst import Burst
 import  matplotlib.pyplot as plt 
 import csv
-print('works')
+import os
+import numpy as np
 
-with open('/home/thomas/data/batse/ascii_data/64ms/burst_files.csv', newline='') as burstfile:
-    base_path = '/home/thomas/data/batse/ascii_data/64ms/'
-    for row in csv.reader(burstfile, delimiter=',', quotechar='|'):
 
-        # if int(row[0]) < 3580:
-        if int(row[0]) != 7812:
-            next
-        else:
-            file_path = base_path+row[1]
-            print(file_path)
+def fix_background(burst_data,time,t90_start,t90_end):
+    # print(burst_data)
+    # print(time)
+    print('len(burst_data)',len(burst_data))
+    burst_data = burst_data[(time < t90_start) | (time > t90_end)]
+    print('len(time)',len(time))
+    time = time[(time < t90_start) | (time > t90_end)]
+    time_fixed = time[burst_data > 0]
+    print('len(time_fixed)',len(time_fixed))
+    burst_fixed = burst_data[burst_data > 0]
+    print('len(burst_fixed)',len(burst_fixed))
+    # plt.plot(time_fixed,burst_fixed,)
+    # # plt.plot(time,burst_data)
+    # plt.show()
 
-            grb = Burst(file_path)
-            grb.parse_batse_file()
+    
+    return burst_fixed,time_fixed
 
-            burst_data = grb.sum_chan_data
 
-            plt.plot(burst_data)
-            plt.show()
+# burst_num = str(5545)
+burst_num = str(1419)
+# burst_num = str(8084)
+
+data_path = os.path.join('..','batse_data')
+
+burst_dict = {}
+with open(os.path.join('data','burst_info.csv'), newline='') as f:
+    for row in csv.DictReader(f, delimiter=','):
+        burst_dict[str(row['burst_num'])] = row
+
+dur_dict = {}
+with open(os.path.join('data','duration_table.csv'), newline='') as f:
+    for row in csv.DictReader(f, delimiter=','):
+        dur_dict[str(row['trig'])] = row
+
+
+burst_info = burst_dict[burst_num]
+# burst_num,burst_path,single_emission
+
+file_path = os.path.join(data_path,burst_info['burst_file'])
+
+grb = Burst(file_path)
+grb.parse_batse_file()
+
+burst_data = grb.sum_chan_data
+header_names = grb.header_names.split()
+header_data = grb.header_data.split()
+
+meta_dict = {}
+for i in range(len(header_data)):
+    meta_dict[header_names[i]] = int(header_data[i])
+
+time = (np.arange(meta_dict['npts'])-meta_dict['nlasc'])*0.064
+
+t90_start = float(dur_dict[burst_num]['t90_start'])-float(dur_dict[burst_num]['t90_err'])
+t90_end = float(dur_dict[burst_num]['t90_start'])+float(dur_dict[burst_num]['t90'])+float(dur_dict[burst_num]['t90_err'])
+
+print(t90_start)
+print(t90_end)
+
+fixed_background, fixed_time = fix_background(burst_data,time,t90_start,t90_end)
+
+
+
+print(meta_dict)
+print(burst_dict[burst_num])
+print(dur_dict[burst_num])
+plt.plot(time,burst_data)
+plt.plot(fixed_time,fixed_background)
+# plt.plot(time,burst_data)
+plt.show()
---
+++ GRBCluster/show_burst.py
@@ -0,0 +1,24 @@
+from grbpy.burst import Burst
+import  matplotlib.pyplot as plt 
+import csv
+import os
+
+with open(os.path.join('data','burst_info.csv'), newline='') as burstfile:
+    base_path = os.path.join('..','batse_data')
+    for row in csv.DictReader(burstfile, delimiter=','):
+        # burst_num,burst_path,single_emission
+
+        # if int(row[0]) < 3580:
+        if int(row['burst_num']) != 7812:
+            next
+        else:
+            file_path = os.path.join(base_path,row['burst_file'])
+            print(file_path)
+
+            grb = Burst(file_path)
+            grb.parse_batse_file()
+
+            burst_data = grb.sum_chan_data
+
+            plt.plot(burst_data)
+            plt.show()
