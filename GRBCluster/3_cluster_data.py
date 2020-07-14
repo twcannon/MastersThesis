c703c43e461e7965c20a394137ffb7a131d151fb
twcannon <tcannon.mail@gmail.com>
2020-07-13 22:41:56 -0400

    started paper

--- GRBCluster/3_cluster_data.py
+++ GRBCluster/3_cluster_data.py
@@ -7,19 +7,14 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 import sys
 sys.setrecursionlimit(10000)
 
+matrix_type = 'norm'
+no_buffer = True
 
-with open(os.path.join('data','norm_matrix.pkl'), 'rb') as f:
-# with open(os.path.join('data','dtw_matrix.pkl'), 'rb') as f:
-# with open(os.path.join('data','euclid_matrix.pkl'), 'rb') as f:
-# with open(os.path.join('data','inv_corr_matrix.pkl'), 'rb') as f:
+with open(os.path.join('data',matrix_type+'_matrix'+('_no_buffer' if no_buffer else '')+'.pkl'), 'rb') as f:
     distance_matrix = pickle.load(f)
 
-with open(os.path.join('data','norm_burst_list.pkl'), 'rb') as f:
-# with open(os.path.join('data','dtw_burst_list.pkl'), 'rb') as f:
-# with open(os.path.join('data','euclid_burst_list.pkl'), 'rb') as f:
-# with open(os.path.join('data','burst_list.pkl'), 'rb') as f:
+with open(os.path.join('data',matrix_type+'_burst_list'+('_no_buffer' if no_buffer else '')+'.pkl'), 'rb') as f:
     burst_list = pickle.load(f)
-
 
 # distance_matrix = 1/np.asarray(distance_matrix)
 
@@ -32,7 +27,8 @@ Z = linkage(distance_matrix, 'single',optimal_ordering=True)
 
 
 fig = plt.figure(figsize=(25, 10))
-dn = dendrogram(Z, p=200, labels = burst_list,distance_sort='descending', truncate_mode='lastp')
+dn = dendrogram(Z, p=200, labels = burst_list, distance_sort='descending')
+# dn = dendrogram(Z, p=200, labels = burst_list,distance_sort='descending', truncate_mode='lastp')
 # dn = dendrogram(Z, labels = burst_list, truncate_mode='lastp')
 # dn = dendrogram(Z, labels = burst_list, truncate_mode='level')
 plt.show()
--- GRBCluster/show_burst.py
+++ GRBCluster/show_burst.py
@@ -43,7 +43,53 @@ with open(os.path.join('data','burst_info.csv'), newline='') as burstfile:
         # if int(row['burst_num']) in [6619,1416,5305]: # ????
         # if int(row['burst_num']) in [6396,2430]: # noisy single with bumps
         # if int(row['burst_num']) in [6295,2061,3248]: # ????
-        if int(row['burst_num']) in [2924,6205,1298]: # noisy single with bumps
+
+
+
+        # corr_norm
+        # if int(row['burst_num']) in [3248,2315,3091]: # ????
+        # if int(row['burst_num']) in [3292,6295,5654]: # ????
+        # if int(row['burst_num']) in [5716,8079]: # singles
+        # if int(row['burst_num']) in [7187,7775,5711]: # ????
+
+
+        # corr_norm no buffer
+        # if int(row['burst_num']) in [5654,398]#,6295,1559]: # double
+        # if int(row['burst_num']) in [3248,2709,7614,6039]: # sharp / noisy singles
+        # if int(row['burst_num']) in [6205,7455]: # short single
+        # if int(row['burst_num']) in [7496,973,5571]: # ????
+        # if int(row['burst_num']) in [108,3286,1953]: # short noisy double
+
+
+
+        # dtw no buffer
+        # does not work well with noisy bursts
+        # if int(row['burst_num']) in [7446,2611,7172]: # clean single with trailing bump
+        # if int(row['burst_num']) in [2151,6904,503]: # ????
+        # if int(row['burst_num']) in [3765,3891,3067,3178]: # bright spike on top of other emission
+        #    if int(row['burst_num']) in [3765,3891]: # bright spike on top of other emission
+        # if int(row['burst_num']) in [6100,1085,6630]: # bright?
+        # if int(row['burst_num']) in [2798,3523]: # bright with bump
+        # if int(row['burst_num']) in [7805,8079,2924,7187]: # ????
+        # if int(row['burst_num']) in [1709,8050]: # ????
+        # if int(row['burst_num']) in [2329,7932,1122]: # many spikes
+        # if int(row['burst_num']) in [2431,2799]: # canonical residuals
+        # if int(row['burst_num']) in [5614,3242]: # ????
+
+
+
+        # norm no buffer
+        # if int(row['burst_num']) in [6113,6904,2151]: # ????
+        # if int(row['burst_num']) in [7028,7781,7172,5470,2367]: # ???? bright stuff
+        # if int(row['burst_num']) in [4814,6830]: # single with cutout in middle
+        # if int(row['burst_num']) in [1982,3130,3649]: # ????
+        # if int(row['burst_num']) in [467,7766]: # sharp rise slow decay
+        # if int(row['burst_num']) in [7446,2611]: # single with bump on back
+        # if int(row['burst_num']) in [7205,6204,2393,6090]: # single with bump on front
+        # if int(row['burst_num']) in [8121,2603,1406]: # canonical single
+        # if int(row['burst_num']) in [7822,3866,4312,2458]: # PULSE EVOLUTION
+        # if int(row['burst_num']) in [3815,752]: # single sharp fall
+        if int(row['burst_num']) in [1472,829]: # single with dip in middle
             file_path = os.path.join(base_path,row['burst_file'])
             print(file_path)
 
